1b9d72e18a5b7eb07bb807567a2b8953a09f8f5d
Gudjon <gudjon17@ru.com>
2019-02-20 14:04:16 +0000

    realloc2 test

--- mm.c
+++ mm.c
@@ -162,7 +162,7 @@ int mm_init(void)
 void *mm_malloc(size_t size) 
 {   
     mm_checkheap(1);
-    HEAPCHECK(0);      /* Ekki gleyma að kommenta út þegar við skilum */
+    //HEAPCHECK(0);      /* Ekki gleyma að kommenta út þegar við skilum */
     size_t asize;      /* adjusted block size */
     size_t extendsize; /* amount to extend heap if no fit */
     char *bp;      
@@ -197,7 +197,7 @@ void *mm_malloc(size_t size)
 /* $begin mmfree */
 void mm_free(void *bp)
 {       
-    HEAPCHECK(0);      /* Ekki gleyma að kommenta út þegar við skilum */
+    //HEAPCHECK(0);      /* Ekki gleyma að kommenta út þegar við skilum */
     size_t size = GET_SIZE(HDRP(bp));
 
     PUT(HDRP(bp), PACK(size, 0));
