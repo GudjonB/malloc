13f16d06d1d8d8838c2263b6ee3ff49c21bed04b
Gudjon <gudjon17@ru.com>
2019-02-20 22:06:08 +0000

    revert

--- mm.c
+++ mm.c
@@ -421,26 +421,26 @@ static void *coalesce(void *bp)
     if (prev_alloc && next_alloc) {            /* Case 1 */
         return bp;
     }
-    else if(!prev_alloc && !next_alloc) {                                     /* Case 4 */
-        removeFromList(NEXT_BLKP(bp));
-        removeFromList(bp);
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
-            GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
-    }
     else if (prev_alloc && !next_alloc) {      /* Case 2 */
         removeFromList(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size,0));
     }
-    else {                                  /* Case 3 */
+    else if (!prev_alloc && next_alloc) {      /* Case 3 */
         removeFromList(bp);
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else {                                     /* Case 4 */
+        removeFromList(NEXT_BLKP(bp));
+        removeFromList(bp);
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
     
