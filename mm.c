59ef1d45c28ea7ec17254c196ea55d94de470090
dagfinnur <dagfinnur15@ru.is>
2019-02-20 11:59:25 +0000

    debug defines virkar ekki.. en svona virkar þetta

--- mm.c
+++ mm.c
@@ -62,7 +62,7 @@ team_t team = {
 • Do any allocated blocks overlap?
 • Do the pointers in a heap block point to valid heap addresses?
  */
-/* printf("%s\n, __func__"); */
+/* VIRKAR EKKI -?-*/
 #ifdef DEBUG
     #define HEAPCHECK(verbose) printf("%s\n, __func__"); mm_checkheap(verbose);
 #else
@@ -161,8 +161,8 @@ int mm_init(void)
 /* $begin mmmalloc */
 void *mm_malloc(size_t size) 
 {
-   // mm_checkheap(1);      /* Ekki gleyma að kommenta út þegar við skilum */
-    HEAPCHECK(1);
+    mm_checkheap(1);      /* Ekki gleyma að kommenta út þegar við skilum */
+   // HEAPCHECK(1);
     size_t asize;      /* adjusted block size */
     size_t extendsize; /* amount to extend heap if no fit */
     char *bp;      
@@ -197,8 +197,8 @@ void *mm_malloc(size_t size)
 /* $begin mmfree */
 void mm_free(void *bp)
 {       
-    //mm_checkheap(1);      /* Ekki gleyma að kommenta út þegar við skilum */
-    HEAPCHECK(1);
+    mm_checkheap(1);      /* Ekki gleyma að kommenta út þegar við skilum */
+    //HEAPCHECK(1);
     size_t size = GET_SIZE(HDRP(bp));
 
     PUT(HDRP(bp), PACK(size, 0));
