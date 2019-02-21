991344cc06020f78f0775ebcb98c0a0d6deb66e2
Gudjon <gudjon17@ru.com>
2019-02-21 15:21:22 +0000

    split find fit

--- mm.c
+++ mm.c
@@ -408,7 +408,7 @@ static void *find_fit(size_t asize)
             if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))) && (GET_SIZE(HDRP(bp))-asize) < remainder) {
                 remainder = GET_SIZE(HDRP(bp)) - asize; // the remainder of the block that was not asked for
                 bestFit = bp;
-                if(remainder  <= 5000){ // when the remainder of the block is less then 3600 bits the block is considered goodenough
+                if(remainder  <= 3600){ // when the remainder of the block is less then 3600 bits the block is considered goodenough
                     return bestFit;   // the number 3600 is a multiple of 8 and was found through trial and error
                 }
             }
