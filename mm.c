6195fd9a5905577f8d507dd4a4e27e9d82db2a18
Gudjon <gudjon17@ru.com>
2019-02-21 15:53:42 +0000

    chunk size

--- mm.c
+++ mm.c
@@ -396,19 +396,11 @@ static void *find_fit(size_t asize)
     listNode bp = LISTHEAD->next;
     listNode bestFit = NULL;
     size_t remainder = 9999999; // some huges number
-    // if(asize > 2000){
-    //     for (; bp != NULL; bp = bp->next) {
-    //     if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-    //         return bp;
-    //     }
-    // }
-    // }
-    // else {
         for (; bp != NULL; bp = bp->next) {
             if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))) && (GET_SIZE(HDRP(bp))-asize) < remainder) {
                 remainder = GET_SIZE(HDRP(bp)) - asize; // the remainder of the block that was not asked for
                 bestFit = bp;
-                if(remainder  <= 3600){ // when the remainder of the block is less then 3600 bits the block is considered goodenough
+                if(remainder  <= 3000){ // when the remainder of the block is less then 3600 bits the block is considered goodenough
                     return bestFit;   // the number 3600 is a multiple of 8 and was found through trial and error
                 }
             }
